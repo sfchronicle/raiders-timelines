6837fd6236246177053b5b117de79c4da5c20ded
emma <emmaruthoneill@gmail.com>
2017-03-27 14:49:33 -0700

    things more or less in place

--- src/js/off.js
+++ src/js/off.js
@@ -1,13 +1,8 @@
 require("./lib/social"); //Do not delete
 
-// filling in Gray slideshow
-// [0,1,2,3,4,5].forEach(function(ff){
-//   $("#photo_g0"+ff).append(slideshow_template(eval("photo0"+ff)));
-// });
-
 // photo gallery event listeners
 var gallery0_idx = 0;
-var Ngallery0 = offData.length;
+var Ngallery0 = offData.length-1;
 
 // set up clicking to update map interactive on mobile
 document.getElementById("scroll-right-gallery0").addEventListener("click", function() {
